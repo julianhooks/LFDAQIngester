b0a7277e7e58b8240e3b85f88d89e01c0d103202
julianhooks <julian.delb.hooks@gmail.com>
2026-05-15 17:15:47 -0400

    First attempts at refactoring setup() for better functions

--- src/main.py
+++ src/main.py
@@ -1,7 +1,7 @@
 import logging
 from dataclasses import dataclass
 import os
-from time import time, sleep
+from time import sleep
 from typing import Annotated
 
 from labjack import ljm
@@ -29,40 +29,58 @@ class Instrument:
     IsLabJack: bool
     LabJackPort: Annotated[str,"LabJack connection handle."]
 
-def setup() -> tuple[
-    list[Instrument],
-    Annotated[int,"LabJack connection handle."],
-    questdb.ingress.Sender]:
- 
-    #setup function returns
-    labjackHandle = 0
-    # labjackHandle = getLabJack()
-    instruments = []
-    # instruments = getInstruments()
-    questDBHandle = None
-    # questDBHandle = get
+def setup(labJackHandle: Annotated[int,"LabJack connection handle."]) -> None:
+    # [IN-PROGRESS] set up counters 1 and 2 for flowmeters
+    ljm.eWriteName(labJackHandle,"DIO0_EF_ENABLE",0)
+    ljm.eWriteName(labJackHandle,"DIO0_EF_INDEX",8)
+    ljm.eWriteName(labJackHandle,"DIO0_EF_ENABLE",1)
+    
+    ljm.eWriteName(labJackHandle,"DIO1_EF_ENABLE",0)
+    ljm.eWriteName(labJackHandle,"DIO1_EF_INDEX",8)
+    ljm.eWriteName(labJackHandle,"DIO1_EF_ENABLE",1)
 
-    # [DONE] Connect to QuestDB for queries
-    # getInstruments(dbconfigs) -> list[Instruments]:
+    # enable below and jump DAC1 to DIO0 to test counter
+    # ljm.eWriteName(labJackHandle,"DAC1_FREQUENCY_OUT_ENABLE",1)
+
+def getLabJack(labjackURL: Annotated[str,"URL"]) -> Annotated[int,"LabJack connection handle."]:
+    try:
+        labjackHandle = ljm.openS("T7","ANY","ANY")
+    except ljm.LJMError as error:
+        logging.error(f"Error occured when connecting to LabJack: {error}.")
+        raise error
+    logging.info(f"Connected to LabJack on {ljm.getHandleInfo(labjackHandle)}.")
+    return labjackHandle
+
+def getInstruments(dbURL: Annotated[str,"URL"]) -> list[Instrument]:
+    # Connect to QuestDB for queries
+    
+    instruments = []
+    functionNamespace = {}
+
+    # [TO-DO] make these env variables 
     with pg.connect(
-            host=dburl,
+            host=dbURL,
             port=8812, 
             user='admin', 
             password='quest',
             dbname='LiquidsTestStand', 
-            autocommit=True) as connector:
-        with connector.cursor(binary=True, row_factory=pg.rows.dict_row) as cursor:
+            autocommit=True
+            ) as connector:
+        with connector.cursor(
+            binary=True, 
+            row_factory=pg.rows.dict_row
+            ) as cursor:
             cursor.execute("SELECT version")
             version = cursor.fetchone()
             logging.info(f'Connected to QuestDB version: {version["version"]}')
-    
-    # [IN-PROGRESS] pull active instruments (labjack port + calibration eq + database stuff) from database
+           
+            # Pull all instruments (labjack port + calibration eq + database stuff) from database
             cursor.execute("SELECT * FROM Instruments")
             instrumentTable = cursor.fetchall()
+          
             for row in instrumentTable:
                 if (bool(row["IsLabJack"]) == False or bool(row["IsActive"]) == False):
                     continue
-                functionNamespace = {}
                 exec("cf = "+ row["CalibrationFunction"],functionNamespace)
                 instruments.append(
                         Instrument(
@@ -73,15 +91,18 @@ def setup() -> tuple[
                             row["Unit"],
                             bool(row["IsLabJack"]),
                             row["LabJackPort"]))
+    return instruments
 
-
-    # [TO-DO] connect to QuestDB for sending
-    # we do want to use http here, not tcp, because for functionality is available for http
+# [DONE] connect to QuestDB for sending
+def getQuestDBHandle(dbURL: Annotated[str,"URL"]) -> questdb.ingress.Sender:
+    # we do want to use http here, not tcp, because more functionality is available for http
     # this uses the influx line protocol, not postgres
+    
+    # [TO-DO] make these environment variables
     try:
         questDBHandle = questdb.ingress.Sender(
             questdb.ingress.Protocol.Http, 
-            dburl, 
+            dbURL, 
             9000, 
             username='admin', 
             password='quest')
@@ -89,44 +110,10 @@ def setup() -> tuple[
     except questdb.ingress.IngressError as error:
         logging.error(f"Error occured when connecting to questDB: {error}.")
         raise error
-        
-
-    # [DONE] connect to Labjack
-    # getLabJackHandle() -> int:
-    try:
-        labjackHandle = ljm.openS("T7","ANY","ANY")
-    except ljm.LJMError as error:
-        logging.error(f"Error occured when connecting to LabJack: {error}.")
-        raise error
-    logging.info(f"Connected to LabJack on {ljm.getHandleInfo(labjackHandle)}.")
-
-    # [IN-PROGRESS] set up counters 1 and 2 for flowmeters
-    ljm.eWriteName(labjackHandle,"DIO0_EF_ENABLE",0)
-    ljm.eWriteName(labjackHandle,"DIO0_EF_INDEX",8)
-    ljm.eWriteName(labjackHandle,"DIO0_EF_ENABLE",1)
     
-    ljm.eWriteName(labjackHandle,"DIO1_EF_ENABLE",0)
-    ljm.eWriteName(labjackHandle,"DIO1_EF_INDEX",8)
-    ljm.eWriteName(labjackHandle,"DIO1_EF_ENABLE",1)
+    # [TO-DO] set up auto-flushing settings for this handle
 
-    # enable below and jump DAC1 to DIO0 to test counter
-    # ljm.eWriteName(labjackHandle,"DAC1_FREQUENCY_OUT_ENABLE",1)
-    
-    questDBHandle.__enter__()
-    return instruments, labjackHandle, questDBHandle
-    
-# [IN-PROGRESS] Exit cleanly on error (+ give me logs of what's going on) 
-def onexit(labjackHandle: Annotated[int,"LabJack connection handle."], questDBHandle: questdb.ingress.Sender) -> None:
-    try:
-        ljm.close(labjackHandle)
-        questDBHandle.close()
-        logging.info("Closed QuestDB, closed LabJack")
-    except ljm.LJMError as error:
-        logging.error(f"Error occured when disconnecting from LabJack: {error}.")
-        raise error
-    except questdb.ingress.IngressError as error:
-        logging.error(f"Error occured when connecting to questDB: {error}.")
-        raise error
+    return questDBHandle
 
 # [DONE] Perform data ingestion:
 # - Get voltages of each active instrument
@@ -150,14 +137,30 @@ def ingestLoop(instruments: list[Instrument],
     questDBHandle.flush()
     sleep(loopDelayms/1000.0)
 
-def main() -> None:
-    instruments, labJackHandle, questDBHandle = setup()
+# [IN-PROGRESS] Exit cleanly on error (+ give me logs of what's going on) 
+def onexit(labjackHandle: Annotated[int,"LabJack connection handle."], questDBHandle: questdb.ingress.Sender) -> None:
     try:
-        while(True):
-            ingestLoop(instruments,labJackHandle,questDBHandle)
-    finally:
-        onexit(labJackHandle,questDBHandle)
+        ljm.close(labjackHandle)
+        logging.info("Closed QuestDB, closed LabJack")
+    except ljm.LJMError as error:
+        logging.error(f"Error occured when disconnecting from LabJack: {error}.")
+        raise error
+
+def main() -> None:
+    
+    labJackHandle = getLabJack(labjackURL)
+    instruments = getInstruments(dburl)
+    
+    setup(labJackHandle)
+
+    with getQuestDBHandle(dburl) as questDBHandle:
+        try:
+            while(True):
+                ingestLoop(instruments,labJackHandle,questDBHandle)
+        finally:
+            onexit(labJackHandle,questDBHandle)
     return
 
 if (__name__ == "__main__"):
     main()
+
