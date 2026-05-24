f77ca873938dc8d7aa564ed8ae440205ba861e3b
julianhooks <julian.delb.hooks@gmail.com>
2026-05-24 15:22:38 -0400

    More work on ingester tests, added bsic fixturing and did research on available testing functions in the ljm library

--- src/ingester.py
+++ src/ingester.py
@@ -2,7 +2,7 @@ import logging
 import os
 import unittest
 from time import sleep
-from typing import Annotated
+from typing import Annotated, Any
 
 from labjack import ljm
 import questdb.ingress
@@ -60,10 +60,56 @@ def onExit(labjackHandle: Annotated[int,"LabJack connection handle."]) -> None:
 class ingesterTest(unittest.TestCase):
 
     def runTest(self):
-        # loop is calling calibration functions properly
-        # loop is calling ereadvoltages properly
-        # loop is writing to db properly
-        # loop is waiting properly
+        # setup() is not accessing false register names
+        # - ljm.nameToAddress should return an error if name does not exist
+        #   - Raises error 1294 on bad name
+        # - ljm.addressToType could also be used to check inputs
+        #   - This returns a number corresponding to an LJM constant for the ctype
+        # - ljm.errorToString might also be useful in the future
+        
+        # Loop is calling calibration functions properly
+        # Loop is calling eReadVoltages properly
+        # Loop is writing to db properly
+        # Loop is waiting properly
+        
+        # Set relevant environment variable
+
+        # Set up instrument list
+        instruments = [Instrument(
+
+        )]
+
+        # Set up false labjack functions
+
+        def falseEReadName(handle: Any,name: Any) -> float:
+            # Do nothing here or check inputs
+            return
+        
+        ljm.eReadName = falseEReadName
+
+        # Set up false Sender object
+
+        class FalseSender(questdb.ingress.Sender):
+            
+            def row(
+                    table_name: str,
+                    *,
+                    symbols: dict[str, str] | None = None,
+                    columns: dict[str, Any] | None = None,
+                    at) -> questdb.ingress.Sender:
+                # Do nothing here or check inputs
+                return self
+
+        dbHandle = FalseSender() 
+        
+        # Run test
+        try:
+            ingestLoop()
+        except Exception as e:
+            pass 
+        
+        # Teardown as necessary
+
         return
 
 
