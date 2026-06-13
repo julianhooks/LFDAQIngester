10450c066f9a7141f360651b00a1cda882de17b6
julianhooks <julian.delb.hooks@gmail.com>
2026-06-13 04:08:51 -0400

    fixing formatting on instrument.py

--- src/lfdaq_ingester/instrument.py
+++ src/lfdaq_ingester/instrument.py
@@ -1,10 +1,10 @@
+"""
+instrument docstring
+"""
 import logging
 from dataclasses import dataclass
 import os
 from typing import Annotated
-import unittest
-import docker
-import time
 
 import psycopg as pg
 
@@ -12,6 +12,9 @@ logger = logging.getLogger(__name__)
 
 @dataclass
 class Instrument:
+    """
+    dataclass docstring
+    """
     InstrumentID: Annotated[str,"QuestDB Symbol"]
     InstrumentName: str
     CalibrationFunction: Annotated[callable,"With functions included from calibration.py"]
@@ -21,38 +24,41 @@ class Instrument:
     LabJackPort: Annotated[str,"LabJack connection handle."]
 
 class InstrumentCreator:
+    """
+    instrument factory docstring
+    """
     def get_instruments(self) -> list[Instrument]:
+        """
+        get instruments docstring
+        """
         instruments = []
-        functionNamespace = {}
-    
+        function_namespace = {}
         with pg.connect(
                 host=os.getenv("LFDAQ_DB_URL"),
-                port=int(os.getenv("LFDAQ_DB_PG_PORT")), 
-                user=os.getenv("LFDAQ_DB_USERNAME"), 
+                port=int(os.getenv("LFDAQ_DB_PG_PORT")),
+                user=os.getenv("LFDAQ_DB_USERNAME"),
                 password=os.getenv("LFDAQ_DB_PASSWORD"),
-                dbname=os.getenv("LFDAQ_DB_NAME"), 
+                dbname=os.getenv("LFDAQ_DB_NAME"),
                 autocommit=True
                 ) as connector:
             with connector.cursor(
-                binary=True, 
+                binary=True,
                 row_factory=pg.rows.dict_row
                 ) as cursor:
                 cursor.execute("SELECT version")
                 version = cursor.fetchone()
                 logger.info(f'Connected to QuestDB version: {version["version"]}')
-               
                 cursor.execute("SELECT * FROM Instruments")
-                instrumentTable = cursor.fetchall()
-              
-                for row in instrumentTable:
-                    if (bool(row["IsLabJack"]) == False or bool(row["IsActive"]) == False):
+                instrument_table = cursor.fetchall()
+                for row in instrument_table:
+                    if (bool(row["IsLabJack"]) is False or bool(row["IsActive"]) is False):
                         continue
-                    exec("cf = "+ row["CalibrationFunction"],functionNamespace)
+                    exec("cf = "+ row["CalibrationFunction"],function_namespace)
                     instruments.append(
                             Instrument(
                                 row["InstrumentID"],
                                 row["InstrumentName"],
-                                functionNamespace["cf"],
+                                function_namespace["cf"],
                                 bool(row["IsActive"]),
                                 row["Unit"],
                                 bool(row["IsLabJack"]),
