aa4214b20c9988a51ebc885f9f384c21ffa82118
julianhooks <julian.delb.hooks@gmail.com>
2026-06-13 03:01:30 -0400

    Matching tests to refactor

--- src/tests/test_ingester.py
+++ src/tests/test_ingester.py
@@ -7,14 +7,25 @@ from typing import Annotated, Any
 from labjack import ljm
 import questdb.ingress
 
-from lfdaq_ingester.instrument import Instrument
-from lfdaq_ingester.ingester import *
+from lfdaq_ingester.ingester import Ingester
 
 logger = logging.getLogger(__name__)
 
 class ingesterTest(unittest.TestCase):
+    def setUp(self):
+        # Set up questdb docker container 
+
+        # Set relevant environment variables
+
+        # Set up ingester for testing
+        self.ingester = Ingester()
+
+        # Set up instrument lists for querying
+
+        # Set up mock labjack functions
 
     def runTest(self):
+        # Run test
         # setup() is not accessing false register names
         # - ljm.nameToAddress should return an error if name does not exist
         #   - Raises error 1294 on bad name
@@ -26,44 +37,15 @@ class ingesterTest(unittest.TestCase):
         # Loop is calling eReadVoltages properly
         # Loop is writing to db properly
         # Loop is waiting properly
-        
-        # Set relevant environment variable
 
-        # Set up instrument list
-        instruments = []
-
-        # Set up false labjack functions
-
-        def falseEReadName(handle: Any,name: Any) -> float:
-            # Do nothing here or check inputs
-            return
-        
-        ljm.eReadName = falseEReadName
-
-        # Set up false Sender object
-
-        class FalseSender(questdb.ingress.Sender):
-            
-            def row(
-                    table_name: str,
-                    *,
-                    symbols: dict[str, str] | None = None,
-                    columns: dict[str, Any] | None = None,
-                    at) -> questdb.ingress.Sender:
-                # Do nothing here or check inputs
-                return self
-
-        dbHandle = FalseSender() 
-        
-        # Run test
         try:
-            ingestLoop()
-        except Exception as e:
-            pass 
-        
-        # Teardown as necessary
+            self.ingester.questdb_handle.establish() 
+        except Exception as error:
+            raise error 
 
-        return
+
+    def tearDown(self):
+        self.ingester.exit() 
 
 
 if (__name__ == "__main__"):
