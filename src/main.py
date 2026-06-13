cb39c83e566360db9d7e0f23abd661bf4e1981eb
julianhooks <julian.delb.hooks@gmail.com>
2026-06-12 21:32:49 -0400

    Made main.py pass python default linting

--- src/main.py
+++ src/main.py
@@ -1,5 +1,7 @@
+"""
+Contains logger set up and main loop code.
+"""
 import logging
-import os
 
 # Import necessary functions
 from lfdaq_ingester.questdb_handle import getQuestDBHandle
@@ -10,35 +12,31 @@ from lfdaq_ingester.instrument import getInstruments
 # Start logger
 logger = logging.getLogger(__name__)
 
-logFormatter = logging.Formatter('%(asctime)s   :%(levelname)s:%(name)s: %(message)s')
+log_formatter = logging.Formatter('%(asctime)s   :%(levelname)s:%(name)s: %(message)s')
 
-logFile = logging.FileHandler(f'{__name__}.log')
-logFile.setLevel(logging.DEBUG)
-logFile.setFormatter(logFormatter)
-logger.addHandler(logFile)
+log_file = logging.FileHandler(f'{__name__}.log')
+log_file.setLevel(logging.DEBUG)
+log_file.setFormatter(log_formatter)
+logger.addHandler(log_file)
 
-logStream = logging.StreamHandler()
-logStream.setLevel(logging.DEBUG)
-logStream.setFormatter(logFormatter)
-logger.addHandler(logStream)
-
+log_stream = logging.StreamHandler()
+log_stream.setLevel(logging.DEBUG)
+log_stream.setFormatter(log_formatter)
+logger.addHandler(log_stream)
 logger.info("Started Logging")
-    
 def main() -> None:
-    
+    """
+    Runs the main lfdaq_ingester loop.
+    """
     instruments = getInstruments()
-    labJackHandle = getLabJack()
-    
-    setup(labJackHandle)
-
-    with getQuestDBHandle() as questDBHandle:
+    lj_handle = getLabJack()
+    setup(lj_handle)
+    with getQuestDBHandle() as db_handle:
         try:
-            while(True):
-                ingestLoop(instruments,labJackHandle,questDBHandle)
+            while True:
+                ingestLoop(instruments,lj_handle,db_handle)
         finally:
-            onExit(labJackHandle)
-    return
+            onExit(lj_handle)
 
-if (__name__ == "__main__"):
+if __name__ == "__main__":
     main()
-
