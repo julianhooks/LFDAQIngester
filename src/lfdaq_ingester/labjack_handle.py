c79e4da3213a608a5fa3e8d2a16c5458496e9130
julianhooks <julian.delb.hooks@gmail.com>
2026-06-13 02:27:52 -0400

    Removing non-class based stuff from module files

--- src/lfdaq_ingester/labjack_handle.py
+++ src/lfdaq_ingester/labjack_handle.py
@@ -29,12 +29,3 @@ class LabJackHandle:
 
     def __exit__(self):
         self.close()
-
-def getLabJack() -> Annotated[int,"LabJack connection handle."]:
-    try:
-        labjackHandle = ljm.openS("T7","ANY","ANY")
-    except ljm.LJMError as error:
-        logger.error(f"Error occured when connecting to LabJack: {error}.")
-        raise error
-    logger.info(f"Connected to LabJack on {ljm.getHandleInfo(labjackHandle)}.")
-    return labjackHandle
--- src/lfdaq_ingester/questdb_handle.py
+++ src/lfdaq_ingester/questdb_handle.py
@@ -21,24 +21,3 @@ class QuestDBHandle(questdb.ingress.Sender):
             auto_flush_interval= int(os.getenv("LFDAQ_DB_AUTOFLUSH_INTERVAL_MS")),
             auto_flush_rows=int(os.getenv("LFDAQ_DB_AUTOFLUSH_ROWS")))
         logger.info(f"Connected to QuestDB influx port")
-
-def getQuestDBHandle() -> questdb.ingress.Sender:
-    # we do want to use http here, not tcp, because more functionality is available for http
-    # this uses the influx line protocol, not postgres
-    
-    try:
-        questDBHandle = questdb.ingress.Sender(
-            questdb.ingress.Protocol.Http, 
-            os.getenv("LFDAQ_DB_URL"), 
-            int(os.getenv("LFDAQ_DB_INFLUX_PORT")), 
-            username=os.getenv("LFDAQ_DB_USERNAME"), 
-            password=os.getenv("LFDAQ_DB_PASSWORD"),
-            auto_flush=True,
-            auto_flush_interval= int(os.getenv("LFDAQ_DB_AUTOFLUSH_INTERVAL_MS")),
-            auto_flush_rows=int(os.getenv("LFDAQ_DB_AUTOFLUSH_ROWS")))
-        logger.info(f"Connected to QuestDB influx port")
-    except questdb.ingress.IngressError as error:
-        logger.error(f"Error occured when connecting to questDB: {error}.")
-        raise error
-    
-    return questDBHandle
