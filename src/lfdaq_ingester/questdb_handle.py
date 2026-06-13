24b7943a6cab0845c03c410c3afe8b07fa167f18
julianhooks <julian.delb.hooks@gmail.com>
2026-06-13 04:21:03 -0400

    fixing formatting on questdb_handle.py

--- src/lfdaq_ingester/questdb_handle.py
+++ src/lfdaq_ingester/questdb_handle.py
@@ -1,23 +1,28 @@
+"""
+Hides influx set up in a subclass of the ingress sender
+"""
 import logging
 import os
-import unittest
-import time
 
-import docker
-from labjack import ljm
 import questdb.ingress
 
 logger = logging.getLogger(__name__)
 
 class QuestDBHandle(questdb.ingress.Sender):
+    """
+    wrapping questdb settings in a class init
+    """
     def __init__(self):
+        """
+        func
+        """
         super().__init__(
-            questdb.ingress.Protocol.Http, 
-            os.getenv("LFDAQ_DB_URL"), 
-            int(os.getenv("LFDAQ_DB_INFLUX_PORT")), 
-            username=os.getenv("LFDAQ_DB_USERNAME"), 
+            questdb.ingress.Protocol.Http,
+            os.getenv("LFDAQ_DB_URL"),
+            int(os.getenv("LFDAQ_DB_INFLUX_PORT")),
+            username=os.getenv("LFDAQ_DB_USERNAME"),
             password=os.getenv("LFDAQ_DB_PASSWORD"),
             auto_flush=True,
             auto_flush_interval= int(os.getenv("LFDAQ_DB_AUTOFLUSH_INTERVAL_MS")),
             auto_flush_rows=int(os.getenv("LFDAQ_DB_AUTOFLUSH_ROWS")))
-        logger.info(f"Connected to QuestDB influx port")
+        logger.info("Connected to QuestDB influx port")
